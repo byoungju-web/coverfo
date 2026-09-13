c1c5c81d7c33c6e98bf8f20fc973a62ee359533b
byoungju-web <hasin7jk@gmail.com>
2026-09-13 22:09:35 +0900

    Add files via upload

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -349,7 +349,12 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
       >
         <ClientOnly>{() => <Menu />}</ClientOnly>
         <div className="flex flex-col lg:flex-row overflow-y-auto w-full h-full">
-          <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
+          <div
+            className={classNames(
+              styles.Chat,
+              'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] min-h-full lg:h-full',
+            )}
+          >
             {!chatStarted && (
               <div id="intro" className="mt-[14vh] max-w-2xl mx-auto text-center px-4 lg:px-0">
                 <img
