f01598f1d900d59d17845fe6bf3292bde222c9a6
byoungju-web <hasin7jk@gmail.com>
2026-09-21 22:51:23 +0900

    Add files via upload

--- app/routes/chat._index.tsx
+++ app/routes/chat._index.tsx
@@ -21,7 +21,7 @@ export const loader = () => json({});
  */
 export default function Index() {
   return (
-    <div className="flex flex-col h-full w-full bg-bolt-elements-background-depth-1">
+    <div className="flex flex-col h-full w-full overflow-x-hidden bg-bolt-elements-background-depth-1">
       <Header />
       <ClientOnly fallback={<BaseChat />}>{() => <Chat />}</ClientOnly>
     </div>
